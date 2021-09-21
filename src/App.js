82e6a32fe1c8ac0c72954c0cc23846dd9cf3e553
dheeraj123495 <dheerajsp0700@gmail.com>
2021-09-21 08:42:06 +0530

    Resturant

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Resturant from "./component/Resturant";
+
+const App = () => {
+
+  // const [count, setCount] = useState(0)
+
+  // const increment = () => {
+  //   setCount(count + 1)
+  // }
+
+  // const decrement = () =>{
+  //   setCount(count-1)
+  // }
+
+  return (
+    <div>
+      {/* <button onClick={increment}>Dheeraj</button>
+      <span>{count}</span>
+      <button onClick={decrement}>Pawar</button> */}
+      <Resturant />
+    </div>
+  );
+}
+
+export default App;
